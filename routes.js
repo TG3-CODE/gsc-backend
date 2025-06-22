580697c28d7d3724418203c030f0a129cb2fe9d6
TG3-CODE <gayatri092023@gmail.com>
2025-06-21 20:59:41 -0500

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,16 +1,15 @@
 const router = require("express").Router();
-const { getVotes, addVote } = require("./controllers");
+const { getVotes, addVote, removeVote } = require("./controllers");
 
 /*router.post("/votes", (req, res) => {
   const { subject } = req.body;
-
   if (!subject) return res.status(400).send("No subject provided");
   votes[subject] = (votes[subject] || 0) + 1;
-
   return res.status(200).send("Vote counted");
 });*/
 
 router.get("/votes", getVotes);
 router.post("/votes", addVote);
+router.post("/votes/remove", removeVote);
 
 module.exports = router;
