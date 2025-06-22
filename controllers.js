8c6c36c2eb57e03a990f762fa31a840ee4687839
TG3-CODE <gayatri092023@gmail.com>
2025-06-21 21:01:31 -0500

    Update controllers.js(Added removeVote)

--- controllers.js
+++ controllers.js
@@ -29,4 +29,29 @@ const addVote = async (req, res, next) => {
   }
 };
 
-module.exports = { getVotes, addVote };
+const removeVote = async (req, res, next) => {
+  const { name } = req.body;
+  console.log("POST /votes/remove called with:", name);
+
+  if (!name) {
+    return res.status(400).json({ error: "Invalid name" });
+  }
+
+  try {
+    let vote = await Vote.findOne({ name });
+    
+    if (vote && vote.value > 0) {
+      vote.value -= 1;
+      await vote.save();
+      console.log(`Vote removed: ${name} = ${vote.value}`);
+      res.json(vote);
+    } else {
+      console.log(`No votes to remove for: ${name}`);
+      res.status(400).json({ error: "No votes to remove" });
+    }
+  } catch (err) {
+    console.error("Error removing vote:", err);
+    res.status(500).json({ error: "Server error" });
+  }
+};
+module.exports = { getVotes, addVote,removeVote};
